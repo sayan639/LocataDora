ac9a4a8816e7ee8a1e76a86abce3125eff2faffe
Sayan639 <sayantanidey639@gmail.com>
2021-03-27 03:27:32 -0500

    added email field in new OnStartActivity

---
+++ app/src/main/java/com/hobarb/locatadora/activities/OnStartActivity.kt
@@ -0,0 +1,11 @@
+package com.hobarb.locatadora.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnStartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_start)
+    }
+}
